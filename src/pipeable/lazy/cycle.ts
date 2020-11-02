71932c82e0c07859041201ce042e86c031379d19
emnudge <emnudge@gmail.com>
2020-11-01 21:06:07 -0600

    fixed cycle to use cache

--- src/pipeable/lazy/cycle.ts
+++ src/pipeable/lazy/cycle.ts
@@ -1,10 +1,18 @@
 /// makes an infinite iterable from given iterable
 /// DO NOT SPREAD THIS! THIS IS INFINITE!
-function* cycle<T>(iter: Iterable<T>): Iterable<T> {
-	while (true) {
+///
+/// Although with that that brings possible other implications, what if the thing you're cycling is infinite, in which case caching is just a bit of an unecessary memory hog
+function cycle<T>(shouldCache = true) {
+	return function* (iter: Iterable<T>): Iterable<T> {
+		const cache = [];
+
 		for (const item of iter) {
 			yield item;
+			if (shouldCache) cache.push(item);
 		}
+
+		// arrays are not consumed when used as iterables, unlike generators
+		while (true) yield* cache;
 	}
 }
 
--- src/regular/lazy/cycle.ts
+++ src/regular/lazy/cycle.ts
@@ -1,11 +1,17 @@
 /// makes an infinite iterable from given iterable
 /// DO NOT SPREAD THIS! THIS IS INFINITE!
-function* cycle<T>(iter: Iterable<T>): Iterable<T> {
-	while (true) {
-		for (const item of iter) {
-			yield item;
-		}
+///
+/// By default, cycle uses a cache. If you're cycling an infinite iterable (for whatever reason), this will turn to a memory hog. 
+function* cycle<T>(iter: Iterable<T>, shouldCache = true): Iterable<T> {
+	const cache = [];
+
+	for (const item of iter) {
+		yield item;
+		if (shouldCache) cache.push(item);
 	}
+
+	// arrays are not consumed when used as iterables, unlike generators
+	while (true) yield* cache;
 }
 
 export default cycle;
