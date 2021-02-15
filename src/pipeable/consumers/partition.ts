0fca9c06f8e1db76bd5704b4d8da92ba29919a87
EmNudge <emnudge@gmail.com>
2021-02-14 21:15:43 -0500

    updated typings

--- src/pipeable/consumers/partition.ts
+++ src/pipeable/consumers/partition.ts
@@ -2,7 +2,7 @@ import { partition as _partition } from "../../regular";
 
 /// splits iterable into 2 chunks based on result of the boolean function
 export function partition<T>(partFunc: HigherOrderFn<T, boolean>) {
-  return (iter: Iterable<T>): [Iterable<T>, Iterable<T>] => _partition(iter, partFunc)
+  return (iter: Iterable<T>): [T[], T[]] => _partition(iter, partFunc)
 }
 
 export default partition;
